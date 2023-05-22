864f8f5f1813698d9a8e1041953e5dc61d57856a
190041137mehedI <190041137mehedi@gmail.com>
2023-05-22 23:25:07 +0600

    Change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/HomeMain";
 import Error from "./pages/Error";
 import Login from "./components/Login/Login";
+import Register from "./components/Register/Register"
 
 const App = () => {
 
@@ -14,6 +15,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
+        <Route exact path="/Register" component={Register} />
         <Route component={Error} />
       </Switch>
     </>
